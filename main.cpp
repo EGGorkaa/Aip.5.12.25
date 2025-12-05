1822627bda40275b89455efa3bf92d3b24abfad5
EGGorkaa <egor_novikov20004@mail.ru>
2025-12-05 12:44:32 +0300

    add DOT

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
+namespace topit {
+  struct p_t{int x, int y};
+  bool operator==(p_t, p_t);
+  bool operator!=(p_t, p_t);
+  struct f_t{p_t aa, bb};
+}
+
+
 int main()
 {
+  using topit::p_t;
+  p_t a{1,0}, b{1;0};
+  std::cout << (a==b)<<"\n";
 
+}
+
+bool::operator==(p_t a, p_t b){
+  return a.x==b.x && a.y==b.y;
+}
+bool::operator!=(p_t a, p_t b){
+  return !(a==b);
 }
